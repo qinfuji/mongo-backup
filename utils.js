1897bddec8439cbcb0d3c806242309d0722fa5db
U-PC201311230019\Administrator <Administrator@PC201311230019.(none)>
2017-06-25 23:27:16 +0800

    init

--- utils.js
+++ utils.js
@@ -14,16 +14,17 @@ module.exports.getUriInfo = function(url) {
 module.exports.cmdExe = async function(cmd) {
     console.log(cmd)
     return new Promise((resolve, reject) => {
-        exec(cmd, { maxBuffer: 5000 * 1024 }, (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            if (error) {
-                console.log(`${cmd}  error: ${ error }`)
-                reject(Result.fail(`${cmd}  error: ${ error }`))
-                return;
-            }
-            console.log(`${cmd} ok`)
-            resolve(Result.ok(`${cmd} ok`))
-        })
+        // exec(cmd, { maxBuffer: 5000 * 1024 }, (error, stdout, stderr) => {
+        //     console.log(`stdout: ${stdout}`);
+        //     console.log(`stderr: ${stderr}`);
+        //     if (error) {
+        //         console.log(`${cmd}  error: ${ error }`)
+        //         reject(Result.fail(`${cmd}  error: ${ error }`))
+        //         return;
+        //     }
+        //     console.log(`${cmd} ok`)
+        //     resolve(Result.ok(`${cmd} ok`))
+        // })
+        return Result.ok(`${cmd} ok`)
     })
 }
